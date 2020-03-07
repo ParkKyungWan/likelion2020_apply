ef946aa60b4e727aff3dceb6cddeb4a3bff231ec
ParkKyungWan <pkw8117197@gmail.com>
2020-03-07 18:24:05 +0900

    second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn likelion_apply.wsgi --log-file -
--- likelion_apply/settings.py
+++ likelion_apply/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +128,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
